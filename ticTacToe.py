55877bcc858bb4c3c021856f92463fbb3aa9e8b7
Tnguyen304 <tnguyen304@mail.valenciacollege.edu>
2016-02-10 19:35:16 -0500

    TicTacToe
    Note: includes programming exiting prevention.

--- ticTacToe.py
+++ ticTacToe.py
@@ -6,10 +6,33 @@ def printBoard(board):
     #                                                                       #
     # Hint: you can follow the same process that was done in the textbook.  #
     #########################################################################
+    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print('-+-+-')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print('-+-+-')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
 
 def checkWinner(board, player):    
     print('Checking if ' + player + ' is a winner...')
-    
+
+    if board['top-L'] == player and board['top-M'] == player and board['top-R'] == player:
+        return True
+    if board['mid-L'] == player and board['mid-M'] == player and board['mid-R'] == player:
+        return True
+    if board['low-L'] == player and board['low-M'] == player and board['low-R'] == player:
+        return True
+    if board['top-L'] == player and board['mid-L'] == player and board['low-L'] == player:
+        return True
+    if board['top-M'] == player and board['mid-M'] == player and board['low-M'] == player:
+        return True
+    if board['top-R'] == player and board['mid-R'] == player and board['low-R'] == player:
+        return True
+    if board['top-L'] == player and board['mid-M'] == player and board['low-R'] == player:
+        return True
+    if board['low-L'] == player and board['mid-M'] == player and board['top-R'] == player:
+        return True
+    return False
+
     # TO DO #################################################################
     # Write code in this function that checks the tic-tac-toe board          #
     # to determine if the player stored in variable 'player' currently      #
@@ -18,8 +41,18 @@ def checkWinner(board, player):
     # variable 'player' has won. The function should return False           #
     # if the player in the variable 'player' has not won.                   #
     #########################################################################
-    
-    
+def isValidInput(move):
+    isValid = (move == 'top-L')
+    isValid = isValid or move == 'top-M'
+    isValid = isValid or move == 'top-R'
+    isValid = isValid or move == 'mid-L'
+    isValid = isValid or move == 'mid-M'
+    isValid = isValid or move == 'mid-R'
+    isValid = isValid or move == 'low-L'
+    isValid = isValid or move == 'low-M'
+    isValid = isValid or move == 'low-R'  
+    return isValid
+
 def startGame(startingPlayer, board):
     # TO DO #################################################################
     # Add comments to each line in this function to describe what           #
@@ -28,21 +61,29 @@ def startGame(startingPlayer, board):
 
     turn = startingPlayer
     for i in range(9):
+    ##This line allows the user to type 9 valid inputs##
         printBoard(board)
         print('Turn for ' + turn + '. Move on which space?')
+    ##Prints line stating the turn of the player X or O##
         move = input()
+    ##Lets the user input a move##
+        while not isValidInput(move):
+    ##Prevents user from exiting the program due to syntax error##
+            print('Please enter a valid input:')
+            move = input()
         board[move] = turn
+    ##Checks to see who's turn(X or O) it is to go##
         if( checkWinner(board, 'X') ):
             print('X wins!')
             break
         elif ( checkWinner(board, 'O') ):
             print('O wins!')
             break
-    
+    ##Checks for winner##
         if turn == 'X':
             turn = 'O'
         else:
             turn = 'X'
         
     printBoard(board)
-    
+    ##END##
